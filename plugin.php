d6dcff5a395fc74b4dc1d1ce9a5874558aa77801
Mace Ojala <mace.ojala@gmail.com>
2025-11-03 15:13:35 +0100

    Tabs to spaces.

--- plugin.php
+++ plugin.php
@@ -4,8 +4,8 @@
 
 class pluginSponsors extends Plugin
 {
-	public function init()
-	{
+    public function init()
+    {
         $jsondb = json_encode(array(
             'RUSTlab' => 'https://rustlab.rub.de'
         ));
@@ -15,7 +15,7 @@ class pluginSponsors extends Plugin
 
         // Disable default Save and Cancel button.
         $this->formButtons = false;
-	}
+    }
 
     // Method called when a POST request is sent
     public function post()
@@ -46,32 +46,32 @@ class pluginSponsors extends Plugin
         return $this->save();
     }
 
-	// Method called on the settings of the plugin on the admin area
-	public function form()
-	{
+    // Method called on the settings of the plugin on the admin area
+    public function form()
+    {
         global $L;
 
         $html .= '<div class="alert alert-danger" role="alert">';
         $html .= "<p>Hallo 👋 This is " . "<em>" . $this->name() . "</em>" . " plugin speaking. The admin UI feels a bit fragile and limited, but seems to get the job done.</p>";
-        $html .= "<p>Please talk with Mace if there is something.</p>.";
+        $html .= "<p>Please talk with Mace if there is something.</p>";
         $html .= '</div>';
 
         // New sponsors
-		$html .= '<h4 class="mt-3">' . $L->get('Add a new sponsor') . '</h4>';
+        $html .= '<h4 class="mt-3">' . $L->get('Add a new sponsor') . '</h4>';
 
-		$html .= '<div>';
-		$html .= '<label>' . $L->get('Name') . '</label>';
-		$html .= '<input name="sponsorName" type="text" dir="auto" class="form-control" value="" placeholder="e.g. RUSTlab">';
-		$html .= '</div>';
+        $html .= '<div>';
+        $html .= '<label>' . $L->get('Name') . '</label>';
+        $html .= '<input name="sponsorName" type="text" dir="auto" class="form-control" value="" placeholder="e.g. RUSTlab">';
+        $html .= '</div>';
 
-		$html .= '<div>';
-		$html .= '<label>' . $L->get('Url') . '</label>';
-		$html .= '<input name="sponsorURL" type="text" dir="auto" class="form-control" value="" placeholder="e.g. https://rustlab.rub.de/">';
-		$html .= '</div>';
+        $html .= '<div>';
+        $html .= '<label>' . $L->get('Url') . '</label>';
+        $html .= '<input name="sponsorURL" type="text" dir="auto" class="form-control" value="" placeholder="e.g. https://rustlab.rub.de/">';
+        $html .= '</div>';
 
-		$html .= '<div>';
-		$html .= '<button name="addSponsor" class="btn btn-primary my-2" type="submit">' . $L->get('Add') . '</button>';
-		$html .= '</div>';
+        $html .= '<div>';
+        $html .= '<button name="addSponsor" class="btn btn-primary my-2" type="submit">' . $L->get('Add') . '</button>';
+        $html .= '</div>';
 
         // List stored sponsors.
         $jsondb   = $this->getValue('jsondb', $unsanitized = false);
@@ -86,17 +86,17 @@ class pluginSponsors extends Plugin
             $html .= '</div>';
 
             $html .= '<div>';
-			$html .= '<label>' . $L->get('Url') . '</label>';
-			$html .= '<input type="text" dir="auto" class="form-control" value="' . $url . '" disabled>';
-			$html .= '</div>';
+            $html .= '<label>' . $L->get('Url') . '</label>';
+            $html .= '<input type="text" dir="auto" class="form-control" value="' . $url . '" disabled>';
+            $html .= '</div>';
 
-			$html .= '<div>';
-			$html .= '<button name="deleteSponsor" class="btn btn-secondary my-2" type="submit" value="' . $name . '">' . $L->get('Delete') . '</button>';
-			$html .= '</div>';
+            $html .= '<div>';
+            $html .= '<button name="deleteSponsor" class="btn btn-secondary my-2" type="submit" value="' . $name . '">' . $L->get('Delete') . '</button>';
+            $html .= '</div>';
         };
 
-		return $html;
-	}
+        return $html;
+    }
 
     public function siteBodyEnd()
     {
